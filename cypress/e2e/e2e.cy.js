59c9ff0521980ad830669f37330540407c972749
NicolasBercero <nicolango24@gmail.com>
2022-10-20 19:35:47 -0300

    Update e2e.cy.js

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -10,10 +10,15 @@ describe('E2E', () => {
         // type Row(left to right) then Column (bottom to top)
         checkers.movePieceFromXtoY([4,3],[5,4])
 
+        //Make 2nd move
         checkers.movePieceFromXtoY([6,3],[7,4])
 
         //Checks if there is a enemy piece on the space
         checkers.validateLostPiece(6,3)
+
+        //Start a new game
+        checkers.clickRestart()
+
 
     })
 
