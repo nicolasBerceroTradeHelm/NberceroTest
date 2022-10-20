aecb5237bce48a79fb81a313d0a5e993d6e60afe
NicolasBercero <nicolango24@gmail.com>
2022-10-20 19:32:52 -0300

    final commit

---
+++ cypress/e2e/e2e.cy.js
@@ -0,0 +1,20 @@
+import * as checkers from "../fixtures/checkers.js";
+
+describe('E2E', () => {
+    it('Checkers', () => {
+        cy.visit('https://www.gamesforthebrain.com/game/checkers/')
+        //Restart the game
+        checkers.clickRestart()
+
+        //Make 1st move
+        // type Row(left to right) then Column (bottom to top)
+        checkers.movePieceFromXtoY([4,3],[5,4])
+
+        checkers.movePieceFromXtoY([6,3],[7,4])
+
+        //Checks if there is a enemy piece on the space
+        checkers.validateLostPiece(6,3)
+
+    })
+
+})
